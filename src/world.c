67f0827414222d92e706a8d96059dcdd62e70139
murumart <lythr0naxe@gmail.com>
2025-03-11 22:41:58 +0200

    useless sort (pre screw up commit)

--- src/world.c
+++ src/world.c
@@ -5,6 +5,16 @@
 #include "ext/stb_ds.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+
+static i32 compare_cpos_distances_to_world_centre(const void *a, const void *b)
+{
+	const ChunkPosition *ca = a;
+	const ChunkPosition *cb = b;
+	i32 distsa = ca->x * ca->x + ca->y * ca->y + ca->z + ca->z;
+	i32 distsb = cb->x * cb->x + cb->y * cb->y + cb->z + cb->z;
+	return distsa - distsb;
+}
 
 void world_init(World *world)
 {
@@ -17,6 +27,7 @@ void world_init(World *world)
 				arrput(initchunks, pos);
 			}
 
+	qsort(initchunks, arrlenu(initchunks), sizeof(ChunkPosition), compare_cpos_distances_to_world_centre);
 	world->chunkmap = NULL;
 	world_fill_chunk_positions(world, initchunks, arrlenu(initchunks));
 	printf("hmlen is %d!\n", (i32)hmlen(world->chunkmap));
